7ad0c20a6e299065f67ffbeceabf56e40249263f
bjokey <>
2022-07-28 01:37:59 +0100

    Add DECTalk (Moonbase) TTS command

--- bjokeybot/cogs/tts.py
+++ bjokeybot/cogs/tts.py
@@ -3,19 +3,19 @@ from io import BytesIO
 
 import discord
 from aiohttp import request
-from bjokeybot.constants import TIKTOK_URL
+from bjokeybot.constants import DECTALK_URL, TIKTOK_URL
 from bjokeybot.logger import log
 from discord.ext import commands
 
 
 class TTSCog(commands.Cog):
     @commands.command(name="tiktok")
-    async def tiktok(self, ctx: commands.Context) -> None:
-        msg = ctx.message.content.removeprefix("bjokey, tiktok")
-        log.info('%s asked tiktok to say "%s"', ctx.author.name, msg)
+    async def tiktok(self, ctx: commands.Context, *args: str) -> None:
+        msg = " ".join(args)
+        log.info('%s asked the tiktok TTS to say "%s"', ctx.author.name, msg)
         data = {
             "text_speaker": "en_us_001",
-            "req_text": ctx.message.content.removeprefix("bjokey, tiktok"),
+            "req_text": msg,
         }
 
         async with request("POST", TIKTOK_URL, data=data) as r:
@@ -24,3 +24,14 @@ class TTSCog(commands.Cog):
 
         with BytesIO(audio) as f:
             await ctx.reply(file=discord.File(fp=f, filename="tts.mp3"))
+
+    @commands.command(name="moonbase")
+    async def moonbase(self, ctx: commands.Context, *args: str) -> None:
+        msg = " ".join(args)
+        log.info('%s asked the DECTalk TTS to say "%s"', ctx.author.name, msg)
+
+        async with request("GET", DECTALK_URL, params={"text": msg}) as r:
+            with BytesIO(await r.read()) as f:
+                await ctx.reply(file=discord.File(fp=f, filename="tts.wav"))
+
+
