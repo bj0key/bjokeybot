d41ccef944b1111c0ac00f69eba3906075668312
bjokey <>
2022-08-13 18:23:47 +0100

    move fetching of TTS bytes data into their own coroutines

--- bjokeybot/cogs/tts.py
+++ bjokeybot/cogs/tts.py
@@ -8,19 +8,32 @@ from bjokeybot.logger import log
 from discord.ext import commands
 
 
+async def get_tiktok(msg) -> bytes:
+    """Use the tiktok API to get a text-to-speech MP3
+    Returns a bytes object containing the file data."""
+    data = {
+        "text_speaker": "en_us_001",
+        "req_text": msg,
+    }
+    async with request("POST", TIKTOK_URL, data=data) as r:
+        r_json = await r.json()
+    audio = b64decode(r_json["data"]["v_str"])
+    return audio
+
+
+async def get_moonbase(msg) -> bytes:
+    async with request("GET", DECTALK_URL, params={"text": msg}) as r:
+        return await r.read()
+
+
 class TTSCog(commands.Cog):
+
     @commands.command(name="tiktok")
     async def tiktok(self, ctx: commands.Context, *args: str) -> None:
         msg = " ".join(args)
         log.info('%s asked the tiktok TTS to say "%s"', ctx.author.name, msg)
-        data = {
-            "text_speaker": "en_us_001",
-            "req_text": msg,
-        }
 
-        async with request("POST", TIKTOK_URL, data=data) as r:
-            r_json = await r.json()
-        audio = b64decode(r_json["data"]["v_str"])
+        audio = await get_tiktok(msg)
 
         with BytesIO(audio) as f:
             await ctx.reply(file=discord.File(fp=f, filename="tts.mp3"))
@@ -30,6 +43,6 @@ class TTSCog(commands.Cog):
         msg = " ".join(args)
         log.info('%s asked the DECTalk TTS to say "%s"', ctx.author.name, msg)
 
-        async with request("GET", DECTALK_URL, params={"text": msg}) as r:
-            with BytesIO(await r.read()) as f:
-                await ctx.reply(file=discord.File(fp=f, filename="tts.wav"))
+        audio = await get_moonbase(msg)
+        with BytesIO(audio) as f:
+            await ctx.reply(file=discord.File(fp=f, filename="tts.wav"))
