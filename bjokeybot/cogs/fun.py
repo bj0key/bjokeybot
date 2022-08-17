c443841d785c307ff3448f98224928427f697591
bjokey <>
2022-08-17 17:00:39 +0100

    Add "target" and "scran" commands

--- bjokeybot/cogs/fun.py
+++ bjokeybot/cogs/fun.py
@@ -1,7 +1,11 @@
+import asyncio
 import random
+from urllib.parse import urlparse
 from io import BytesIO
 
+
 from bjokeybot.logger import log
+from bjokeybot.constants import SCRAN_CHANNEL
 from discord import File, DiscordException
 from discord.ext import commands
 
@@ -25,7 +29,6 @@ class FunCog(commands.Cog):
     @commands.command(name="femham")
     async def femham(self, ctx: commands.Context) -> None:
         log.info("%s ran femham.", ctx.author.name)
-        hammed = False
         if ctx.message.author.id == 66183829148151808:
             await ctx.reply("Why are you trying to use the command, cris")
             hammed = True
@@ -38,3 +41,44 @@ class FunCog(commands.Cog):
                 await ctx.author.edit(nick="femhammer " + (ctx.author.nick or ctx.author.name))
             except DiscordException as e:
                 log.warn("Femham renaming raised exception %s: %s", e.__class__.__name__, str(e).replace("\n", " "))
+
+    @commands.command(name="target")
+    async def target(self, ctx: commands.Context, t: str) -> None:
+        """Drone strike a target"""
+        log.info("%s targeted %s.", ctx.author.name, t)
+        if t.lower() == "israel":
+            await ctx.reply("https://cdn.discordapp.com/attachments/364933355290099714/1009473800217317407/unknown.png")
+        else:
+            async with ctx.typing():
+                msg = await ctx.reply(f"Targeting {t}...")
+                await asyncio.sleep(2)
+                await msg.edit(
+                    content="https://cdn.discordapp.com/attachments/364933355290099714/1009474594387795980/unknown.png")
+                await asyncio.sleep(2)
+                await msg.edit(content=f"{t} successfully eliminated")
+
+    @commands.command(name="scran")
+    async def get_scran(self, ctx: commands.Context) -> None:
+        """Get a random bit of scran from the scran channel"""
+        # Note: while pulling data live from the channel is cool,
+        # instead reading from a cached list would probably be wiser
+        for channel in ctx.guild.text_channels:
+            if channel.id == SCRAN_CHANNEL:
+                scran_channel = channel
+                break
+        else:
+            log.error("Couldn't find scran channel!")
+            await ctx.reply("⚠ we all outta scran")
+            return
+
+        scran = []
+        async for msg in scran_channel.history(limit=400):
+            if len(msg.attachments) > 0:
+                scran += msg.attachments
+
+            else:
+                # Check to see if message contains a valid URL
+                if urlparse(msg.content).netloc != "":
+                    scran.append(msg.content)
+
+        await ctx.reply(random.choice(scran))
