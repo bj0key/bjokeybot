ddcd62747d800bd91ce6c1c59ec64c1e15b40e00
bjokey <>
2022-08-23 03:47:24 +0100

    Add initialisation datetime to bot

--- bjokeybot/bot.py
+++ bjokeybot/bot.py
@@ -1,9 +1,14 @@
+from datetime import datetime
+
 from discord.ext import commands
 
 from bjokeybot import cogs
 
 
 class BjokeyBot(commands.Bot):
+    def __init__(self):
+        self.start_time = datetime.now()
+        """The time of initialisation, AKA when the bot was started"""
 
     async def setup_hook(self) -> None:
         for cog in cogs.all_cogs:
