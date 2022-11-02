4e3b40a32d632762933a134e826c10f930247e46
bjokey <>
2022-11-02 23:27:50 +0000

    the bot has a more natural presence when awakened

--- bjokeybot/bot.py
+++ bjokeybot/bot.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 from bjokeybot.cogs import all_cogs
@@ -7,3 +8,6 @@ class BjokeyBot(commands.Bot):
     async def setup_hook(self) -> None:
         for cog in all_cogs:
             await self.add_cog(cog(self))
+
+    async def on_ready(self):
+        await self.change_presence(activity=discord.Streaming(name="Fornite, and drinking cola?", url="https://www.twitch.tv/crool_"))
