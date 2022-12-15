02bfeffa0fd590e7c2771d8d293bf9e5f5c231ae
bjokey <>
2022-12-15 06:30:48 +0000

    Add `ansi` command, which converts a basic text format to ANSI escape sequences

--- bjokeybot/cogs/util/util.py
+++ bjokeybot/cogs/util/util.py
@@ -1,4 +1,5 @@
 import ast
+import re
 
 from discord.ext import commands
 
@@ -18,6 +19,34 @@ EVAL_WHITELIST = (
     ast.Num,
 )
 
+ANSI_TAGS = {
+    "fxnorm": 0,  # normal text
+    "fxbold": 1,  # bold text
+    "fxundr": 4,  # underlined text
+    "gray": 30,
+    "red": 31,
+    "green": 32,
+    "yellow": 33,
+    "blue": 34,
+    "pink": 35,
+    "cyan": 36,
+    "white": 37,
+    "bgdarkblue": 40,
+    "bgorange": 41,
+    "bgblue": 42,
+    "bgturquoise": 43,
+    "bggray": 44,
+    "bgindigo": 45,
+    "bglgray": 46,
+    "bgwhite": 47
+}
+
+ANSI_PATTERN = re.compile(r"\$\[(\w+?)]")
+
+
+def match_to_esc(match: re.Match[str]) -> str:
+    return f"\x1b[{ANSI_TAGS[match.group(1)]}m"
+
 
 class GeneralUtilityCog(BjokeyCog):
 
@@ -32,3 +61,11 @@ class GeneralUtilityCog(BjokeyCog):
             await ctx.reply("⚠ Couldn't find user!")
         else:
             await ctx.reply(user.display_avatar.url)
+
+    @commands.command(name="ansi")
+    async def ansi(self, ctx: commands.Context, *, message: str) -> None:
+        try:
+            subbed_out = ANSI_PATTERN.sub(match_to_esc, message)
+            await ctx.reply(f"```ansi\n{subbed_out}```")
+        except KeyError as e:
+            await ctx.reply(f"⚠ Uuh, I don't know what colour {e} is meant to be...")
