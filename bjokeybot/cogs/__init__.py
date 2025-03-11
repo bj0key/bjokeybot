6b6c7959e4af9ef1b5fd12dee9e280882ad2ffdb
bj0key <95365351+bj0key@users.noreply.github.com>
2025-03-11 18:42:02 +0000

    add dogging (album club) cog

--- bjokeybot/cogs/__init__.py
+++ bjokeybot/cogs/__init__.py
@@ -1,7 +1,9 @@
+from bjokeybot.cogs.base import BjokeyCog
 from bjokeybot.cogs.fun import FunCog
 from bjokeybot.cogs.stats import StatsCog
 from bjokeybot.cogs.tts import TTSCog
 from bjokeybot.cogs.util import GeneralUtilityCog, MathUtilityCog
 from bjokeybot.cogs.chat_admin import ChatAdminCog
+from bjokeybot.cogs.dogging import DoggingCog
 
-all_cogs = (FunCog, StatsCog, TTSCog, GeneralUtilityCog, MathUtilityCog, ChatAdminCog)
+all_cogs: list[type[BjokeyCog]] = [FunCog, StatsCog, TTSCog, GeneralUtilityCog, MathUtilityCog, ChatAdminCog, DoggingCog]
---
+++ bjokeybot/cogs/dogging.py
@@ -0,0 +1,429 @@
+from datetime import datetime
+from typing import NamedTuple
+
+import aiosqlite
+import discord
+import discord.utils
+from discord import Interaction, app_commands
+from discord.ext import commands
+from discord.utils import escape_markdown
+from io import BytesIO
+from json import dumps
+
+# from bjokeybot.logger import log
+from .base import BjokeyCog
+
+
+class Album(NamedTuple):
+    id: int
+    season: int
+    date: str
+    choice: int
+    artist: str
+    title: str
+
+
+class Rating(NamedTuple):
+    album: int
+    dogger: int
+    score: int
+
+
+DB_FILEPATH = "../dogging.sqlite3"
+DAYS_TO_CHANGE_SCORE = 1
+
+
+def db_conn() -> aiosqlite.Connection:
+    return aiosqlite.connect(DB_FILEPATH)
+
+
+def like(s: str) -> str:
+    return f"%{s}%"
+
+
+def album_factory(cursor: aiosqlite.Cursor, row: aiosqlite.Row) -> Album:
+    return Album(*row)
+
+
+def rating_factory(cursor: aiosqlite.Cursor, row: aiosqlite.Row) -> Rating:
+    return Rating(*row)
+
+
+async def init_tables() -> None:
+    async with db_conn() as conn:
+        await conn.execute(
+            "CREATE TABLE IF NOT EXISTS Albums ("
+            "id INTEGER PRIMARY KEY ASC,"
+            "season INTEGER NOT NULL,"
+            "date TEXT NOT NULL,"
+            "choice INTEGER,"
+            "artist TEXT NOT NULL,"
+            "title TEXT NOT NULL,"
+            "UNIQUE (artist, title)"
+            ");"
+        )
+        await conn.execute(
+            "CREATE TABLE IF NOT EXISTS Ratings ("
+            "album INTEGER REFERENCES Albums (id),"
+            "dogger INTEGER NOT NULL,"
+            "score INTEGER NOT NULL,"
+            "PRIMARY KEY (album, dogger)"
+            ");"
+        )
+
+
+async def fetch_albums_from_title(title: str) -> list[Album]:
+    async with db_conn() as conn:
+        conn.row_factory = album_factory  # type: ignore
+        cur = await conn.execute(
+            "SELECT * FROM Albums WHERE title LIKE ?;", (like(title),)
+        )
+        return list(await cur.fetchall())  # type: ignore
+
+
+async def fetch_album_from_title(title: str) -> Album | None:
+    async with db_conn() as conn:
+        conn.row_factory = album_factory  # type: ignore
+        cur = await conn.execute(
+            "SELECT * FROM Albums WHERE title LIKE ?;", (like(title),)
+        )
+        return await cur.fetchone()  # type: ignore
+
+
+async def fetch_specific_rating(album: Album, user_id: int) -> Rating | None:
+    async with db_conn() as conn:
+        conn.row_factory = rating_factory  # type: ignore
+        cur = await conn.execute(
+            "SELECT * FROM Ratings WHERE album = ? AND dogger = ?", (album.id, user_id)
+        )
+        return await cur.fetchone()  # type: ignore
+
+
+async def add_rating(rating: Rating, *, replace_existing: bool = False) -> None:
+    async with db_conn() as conn:
+        if replace_existing:
+            command = "INSERT OR REPLACE INTO Ratings (album, dogger, score) VALUES (?, ?, ?);"
+        else:
+            command = "INSERT INTO Ratings (album, dogger, score) VALUES (?, ?, ?);"
+        await conn.execute(
+            command,
+            (rating.album, rating.dogger, rating.score),
+        )
+
+
+async def add_album(album: Album) -> None:
+    async with db_conn() as conn:
+        await conn.execute(
+            "INSERT INTO Albums"
+            "(season, date, choice, artist, title)"
+            "VALUES (?, ?, ?, ?, ?);",
+            (
+                album.season,
+                album.date,
+                album.choice,
+                album.artist,
+                album.title,
+            ),
+        )
+        await conn.commit()
+
+
+async def fetch_album_from_id(album_id: int) -> Album | None:
+    async with db_conn() as conn:
+        conn.row_factory = album_factory  # type: ignore
+        cur = await conn.execute("SELECT * FROM Albums WHERE id = ?;", (album_id,))
+        return await cur.fetchone()  # type: ignore
+
+
+async def fetch_ratings(album: Album) -> list[Rating]:
+    async with db_conn() as conn:
+        old_factory = conn.row_factory
+        conn.row_factory = rating_factory  # type: ignore
+        cur = await conn.execute("SELECT * FROM Ratings WHERE album = ?;", (album.id,))
+        ratings = await cur.fetchall()
+        conn.row_factory = old_factory
+    return ratings  # type: ignore
+
+
+class DoggingCog(BjokeyCog):
+    curr_album_id: int | None
+
+    async def cog_load(self) -> None:
+        self.curr_album_id = None
+        await init_tables()
+        await super().cog_load()
+
+    async def album_autocomplete(
+        self, interaction: Interaction, album: str
+    ) -> list[app_commands.Choice]:
+
+        albums = await fetch_albums_from_title(album)
+        choices = [
+            app_commands.Choice(name=f"{a.artist} - {a.title}", value=a.title)
+            for a in albums[:25]
+        ]
+        return choices
+
+    @app_commands.command(
+        name="summary", description="Check the scores for a given album"
+    )
+    @app_commands.autocomplete(album_title=album_autocomplete)
+    async def summary(self, interaction: Interaction, album_title: str) -> None:
+        album = await fetch_album_from_title(album_title)
+        if album is None:
+            await interaction.response.send_message(
+                f"Couldn't find album matching title: {album_title}",
+                ephemeral=True,
+            )
+            return
+        ratings = await fetch_ratings(album)
+        chooser = await self.bot.fetch_user(album.choice)
+        doggers = {r: await self.bot.fetch_user(r.dogger) for r in ratings}
+
+        output = []
+        output.append(
+            f"**{album.artist} - {album.title}** (Chosen by {escape_markdown(chooser.name)})"
+        )
+        output.append(f"Listened to on {album.date}")
+        output.append("====")
+        name_padded_len = max(len(user.name) for user in doggers.values()) + 2
+        for rating, user in doggers.items():
+            output.append(f"`{user.name + ':':<{name_padded_len}}{rating.score}`")
+        if len(ratings) > 0:
+            average = sum(r.score for r in ratings) / len(ratings)
+            output.append(f"\r\n**OVERALL SCORE: {average:.2f}**")
+        else:
+            output.append("\r\n**OVERALL SCORE: ??**")
+
+        await interaction.response.send_message(
+            "\r\n".join(output),
+            ephemeral=False,
+        )
+
+    @app_commands.command(name="rate", description="Add a score to the database")
+    @app_commands.autocomplete(album_title=album_autocomplete)
+    async def rate(
+        self, interaction: Interaction, album_title: str, score: int
+    ) -> None:
+        await interaction.response.defer(ephemeral=True, thinking=True)
+
+        if not 0 <= score <= 100:
+            await interaction.edit_original_response(
+                content=f"{score}/100 is just plain silly, be fr"
+            )
+
+        album = await fetch_album_from_title(album_title)
+        if album is None:
+            await interaction.edit_original_response(
+                content=f"Couldn't find album title in DB: {album_title}\n"
+                "Either you misspelled the title, or bjokey hasnt added it yet"
+            )
+            return
+
+        rating = Rating(album.id, interaction.user.id, score)
+
+        old_rating = await fetch_specific_rating(album, interaction.user.id)
+        if old_rating is None:
+            await add_rating(rating)
+            await interaction.edit_original_response(
+                content=f"Successfully gave {album.artist} - {album.title} a {rating.score}/100.",
+            )
+        else:
+            album_date = datetime.strptime(album.date, "%Y-%m-%d")
+            ago = datetime.now() - album_date
+            if ago.days > DAYS_TO_CHANGE_SCORE:
+                await interaction.edit_original_response(
+                    content=f"You've already rated {album.title}, and it was listened to {ago.days} days ago! Your score is LOCKED IN!!!!"
+                )
+                return
+
+            await interaction.edit_original_response(
+                content=f"⚠ You already have a score for {album.artist} - {album.title} ({old_rating.score}/100). Want to replace it?",
+                view=ReplaceRatingsView(old_rating, rating),
+            )
+
+    @app_commands.command(
+        name="add_album", description="Add an album to the database (bjokey-only)"
+    )
+    async def add_album(self, interaction: Interaction) -> None:
+        if not await self.bot.is_owner(interaction.user):
+            await interaction.response.send_message(
+                "Only bjokey can do this, ask him to add the album", ephemeral=True
+            )
+        else:
+            await interaction.response.send_modal(
+                AddAlbumModal(self.bot, interaction.guild)
+            )
+
+    @commands.command(name="force_rate")
+    @commands.is_owner()
+    async def force_rate(
+        self,
+        ctx: commands.Context,
+        album_title: str,
+        dogger: discord.Member,
+        score: int,
+    ) -> None:
+        album = await fetch_album_from_title(album_title)
+        if album is None:
+            await ctx.reply(f"Bad album title: {album_title}")
+            return
+
+        rating = Rating(album.id, dogger.id, score)
+        await add_rating(rating, replace_existing=True)
+        await ctx.message.add_reaction("✅")
+        await ctx.reply(
+            f"Set rating for {album.title} by {dogger.name} to be {score}/100"
+        )
+
+    @commands.command(name="dogdb")
+    @commands.is_owner()
+    async def export_db_file(self, ctx: commands.Context) -> None:
+        db_file = discord.File(DB_FILEPATH, "dogging.sqlite3")
+        dm = await ctx.author.create_dm()
+        await dm.send(file=db_file)
+
+    @app_commands.command(name="dogjson")
+    async def export_json(self, interaction: Interaction) -> None:
+        data = {}
+        all_albums = await fetch_albums_from_title("")
+        for album in all_albums:
+            album_data = album._asdict()
+            del album_data["id"]
+            scores = await fetch_ratings(album)
+            album_data["scores"] = {s.dogger: s.score for s in scores}
+            data[album.id + 1] = album_data
+        file_data = BytesIO(dumps(data).encode("utf8"))
+        file_data.seek(0)
+        file = discord.File(file_data, filename="dogging.json")
+        await interaction.response.send_message(file=file)
+
+
+class ReplaceRatingsView(discord.ui.View):
+    def __init__(self, old: Rating, new: Rating, *, timeout: float | None = 180):
+        super().__init__(timeout=timeout)
+        self.old = old
+        self.new = new
+
+    @discord.ui.button(label="Overwrite", style=discord.ButtonStyle.blurple)
+    async def overwrite_button(
+        self, interaction: Interaction, button: discord.ui.Button
+    ) -> None:
+        await add_rating(self.new, replace_existing=True)
+        await interaction.response.send_message(
+            f"Score successfully updated from {self.old.score}/100 to {self.new.score}/100.",
+            ephemeral=True,
+        )
+        self.stop()
+
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.gray)
+    async def cancel_button(
+        self, interaction: Interaction, button: discord.ui.Button
+    ) -> None:
+        await interaction.response.send_message(
+            content="Score left unchanged.",
+            ephemeral=True,
+        )
+        self.stop()
+
+
+class AddAlbumModal(discord.ui.Modal, title="Add album"):
+    def __init__(
+        self, bot: commands.Bot, guild: discord.Guild | None, **kwargs
+    ) -> None:
+        super().__init__(**kwargs)
+        self.bot = bot
+        self.guild = guild
+
+    season = discord.ui.TextInput(label="Season", required=True)
+    date = discord.ui.TextInput(label="Date", placeholder="YYYY-mm-dd", required=False)
+    choice = discord.ui.TextInput(
+        label="Choice", placeholder="ID/username", required=True
+    )
+    artist = discord.ui.TextInput(label="Artist", required=True)
+    album_title = discord.ui.TextInput(label="Title", required=True)
+
+    async def get_and_validate_input(self) -> Album:
+        # We're just going through each field, and double-checking that its valid
+        errors = []
+
+        # Check season is an actual integer
+        try:
+            season = int(self.season.value)
+        except ValueError:
+            errors.append("Invalid season number.")
+
+        # Date should be either blank, or parseable into a date of the form YYYY-mm-dd
+        try:
+            if len(self.date.value) > 0:
+                date = datetime.strptime(self.date.value, "%Y-%m-%d")
+            else:
+                date = datetime.now()
+            if date.year < 2020:
+                errors.append(
+                    f"The year value looked wrong ({date.year}), double-check that"
+                )
+            timestamp = date.strftime("%Y-%m-%d")
+        except ValueError:
+            errors.append("Invalid date provided.")
+
+        # Choice should be a valid server member ID or username
+        # this code is horrible btw
+
+        choice: int | str = self.choice.value
+        try:
+            choice = int(choice)
+        except ValueError:
+            pass
+
+        if isinstance(choice, int):
+            # int conversion worked, treat is like a discord ID
+            user = await self.bot.fetch_user(choice)
+            if user is None:
+                errors.append("User ID is not a valid user.")
+            elif self.guild is not None and self.guild.get_member(user.id) is None:
+                errors.append(
+                    "User ID is valid, but not in this server. Run command in DMs to ignore membership test."
+                )
+        else:
+            # int conversion failed, treat it as a username
+            if self.guild is None:
+                errors.append("Cannot fetch user by username in DMs.")
+            else:
+                member = self.guild.get_member_named(choice)
+                if member is None:
+                    errors.append(f"Server member {choice} couldn't be found.")
+                else:
+                    choice_id = member.id
+
+        artist = self.artist.value
+        if len(artist) == 0:
+            errors.append("Artist name is blank")
+
+        title = self.album_title.value
+        if len(title) == 0:
+            errors.append("Album title is blank")
+
+        if len(errors) == 0:
+            return Album(-1, season, timestamp, choice_id, artist, title)
+        else:
+            raise ValueError(errors)
+
+    async def on_submit(self, interaction: Interaction) -> None:
+        try:
+            album = await self.get_and_validate_input()
+        except ValueError as e:
+            err_output = f"Error(s) when adding album:\n{"\n".join(e.args)}"
+            await interaction.response.send_message(content=err_output, ephemeral=True)
+            return
+
+        await add_album(album)
+        await interaction.response.send_message(
+            content=f"Successfully added **{album.artist} - {album.title}** to albums"
+        )
+
+    async def on_error(self, interaction: Interaction, error: Exception) -> None:
+        await interaction.response.send_message(
+            content=f"Error on album adding: {error!r}",
+            ephemeral=True,
+        )
